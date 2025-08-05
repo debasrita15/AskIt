cce3f1d37b3d743d3130b1e9ed57f08a2d071254
Sanskruti Dash <sanskrutidash1808@gmail.com>
2025-08-05 11:36:15 +0530

    Colour changed

--- app/src/main/java/com/example/askit/data/view/SignupScreen.kt
+++ app/src/main/java/com/example/askit/data/view/SignupScreen.kt
@@ -11,11 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+//import com.example.askit.data.viewmodel.AuthViewModel
 import com.example.askit.data.viewmodel.AuthViewModel
-
 @Composable
 fun SignUpScreen(
     onSwitchToSignIn: () -> Unit,
--- app/src/main/java/com/example/askit/ui/theme/Color.kt
+++ app/src/main/java/com/example/askit/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.askit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue80 = Color(0xFFB3D8FF)         // Light Blue (was Purple80)
+val BlueGrey80 = Color(0xFFC1D3E0)      // Light Blue-Grey (was PurpleGrey80)
+val LightBlue80 = Color(0xFFAEDFF7)     // Light Sky Blue (was Pink80)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF1565C0)          // Deep Blue (was Purple40)
+val BlueGrey40 = Color(0xFF546E7A)      // Blue-Grey (was PurpleGrey40)
+val LightBlue40 = Color(0xFF0288D1)     // Lighter Blue (was Pink40)
